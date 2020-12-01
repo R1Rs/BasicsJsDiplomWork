3f98c3ad9874669dea6d0946a85dce9fc97d3e67
Renat <suleymanov_renat@mail.ru>
2020-12-01 13:31:16 +0300

    Fetch

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -12,34 +12,39 @@ const createRequest = (options = {}) => {
     let xhr = new XMLHttpRequest();
     xhr.responseType = responseType;
 
-    xhr.addEventListener("readystatechange", () => {
+    function listener () {
+        xhr.send();
+        xhr.addEventListener("readystatechange", () => {
         console.log(xhr.readyState, xhr.status);
         if (xhr.readyState == 4 && xhr.status == 200) {
             let response = xhr.response;
             console.log(response.success);
             callback(response)
         } else if (xhr.readyState == 4 && xhr.status != 200) {
-            let err = xrh.status;
+            let err = xhr.status;
             console.log(err);
             callback(err);
         } 
     })
-
-
+    }
     if (method == "GET" && data) {
         xhr.open("GET", `${url}?mail=${data.mail}&password=${data.password}`);
-        xhr.send();
+        // xhr.send();
+        listener();
+
     } else if (method == "GET") {
         xhr.open("GET", url);
-        xhr.send();
+        listener();
+
     } else {
         let formData = new FormData;
         formData.append("mail", `${data.mail}`);
         formData.append("password", `${data.password}`);
-        xhr.open(`${method}`, `${url}`); 
-        xhr.send(formData);
+        xhr.open(`${method}`, `${url}`);
+        // xhr.send(formData);
+        listener() ;
+
     }
-    
     function callback (a) {
         console.log(a);
        return a;
